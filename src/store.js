627efa1edbd68b42239d300a8f88f50e912b53d1
Viviana Feijoo <vivfeijoo@gmail.com>
2025-06-03 11:47:06 +0000

    useGlobalReducer

--- src/store.js
+++ src/store.js
@@ -1,32 +1,40 @@
 export const initialStore=()=>{
   return{
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    contacts: []
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
+  switch (action.type) {
+    case 'SET_CONTACTS':
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        contacts: action.payload
       };
+
+    case 'ADD_CONTACT':
+      return {
+        ...store,
+        contacts: [...store.contacts, action.payload]
+      };
+
+    case 'UPDATE_CONTACT':
+      return {
+        ...store,
+        contacts: store.contacts.map(contact =>
+          contact.id === action.payload.id ? action.payload : contact
+        )
+      };
+
+    case 'DELETE_CONTACT':
+      return {
+        ...store,
+        contacts: store.contacts.filter(contact => contact.id !== action.payload)
+      };
+
     default:
-      throw Error('Unknown action.');
-  }    
+      throw new Error('Unknown action type: ' + action.type);
+  }
 }
+  
